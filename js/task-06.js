41d9f78c6620ae002375ac08ff3d84e572385e29
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2019-12-03 01:14:48 +0200

    Fixed bugs

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 let number_user;
-let total;
+let total = 0;
 while (number_user !== null) {
   number_user = prompt('Введите пожалуйста число');
   if (number_user !== null) {
-    total = total + Number(number_user);
+    total = total + number_user;
   } else if (number_user === null) {
     console.log('Общая сумма чисел равна ' + total);
   } else {
