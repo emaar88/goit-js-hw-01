7782506265636d9fdff48e56e236970de305b1d8
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2019-12-02 12:55:47 +0200

    some fixed

--- js/task-05.js
+++ js/task-05.js
@@ -5,23 +5,30 @@ const AustraliaPrise = 170;
 const IndiaPrise = 80;
 const JamaicaPrise = 120;
 
-selectCountry = selectCountry.toLowerCase();
-switch (selectCountry) {
-    case 'китай':
-      alert('Доставка в ' + selectCountry + ' будет стоить ' + ChinaPrise + ' кредитов');
-      break;
-    case 'чили':
-      alert('Доставка в ' + selectCountry + ' будет стоить ' + ChilePrise + ' кредитов');
-      break;
-    case 'австралия':
-      alert('Доставка в ' + selectCountry + ' будет стоить ' + AustraliaPrise + ' кредитов');
-      break;
-    case 'индия':
-      alert('Доставка в ' + selectCountry + ' будет стоить ' + IndiaPrise + ' кредитов');
-      break;
-    case 'ямайка':
-      alert('Доставка в ' + selectCountry + ' будет стоить ' + JamaicaPrise + ' кредитов');
-      break;
-    default:
-      alert('В вашей стране доставка не доступна');
-  }
+if (selectCountry === null)
+{
+  alert('Отменено пользователем');
+}
+else
+{
+  selectCountry = selectCountry.toLowerCase();
+  switch (selectCountry) {
+      case 'китай':
+        alert('Доставка в ' + selectCountry + ' будет стоить ' + ChinaPrise + ' кредитов');
+        break;
+      case 'чили':
+        alert('Доставка в ' + selectCountry + ' будет стоить ' + ChilePrise + ' кредитов');
+        break;
+      case 'австралия':
+        alert('Доставка в ' + selectCountry + ' будет стоить ' + AustraliaPrise + ' кредитов');
+        break;
+      case 'индия':
+        alert('Доставка в ' + selectCountry + ' будет стоить ' + IndiaPrise + ' кредитов');
+        break;
+      case 'ямайка':
+        alert('Доставка в ' + selectCountry + ' будет стоить ' + JamaicaPrise + ' кредитов');
+        break;
+      default:
+        alert('В вашей стране доставка не доступна');
+    }
+}  
