a209ea7648b32eeec06cddef739db8412b4b0794
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2019-12-02 14:33:18 +0200

    New fixed4

--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,15 @@
 const total = prompt('Введите количество товаров на складе');
 const ordered = prompt('Введите количество единиц товара в заказе');
 
-if ((total || ordered) === null)
+if ((total && ordered) !== null)
 {
     if (total < ordered) {
     console.log('"На складе недостаточно товаров!"');
     } else {
     console.log('"Заказ оформлен, с вами свяжется менеджер"');
     }
+}
+else
+{
+    alert('Заполнены должны быть оба поля');
 }
