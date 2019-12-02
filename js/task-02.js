05ca7d2968508915e83e6a488ce028cfb15663b6
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2019-12-02 14:30:22 +0200

    New fixed3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
 const total = prompt('Введите количество товаров на складе');
 const ordered = prompt('Введите количество единиц товара в заказе');
 
-if (total < ordered) {
-  console.log('"На складе недостаточно товаров!"');
-} else {
-  console.log('"Заказ оформлен, с вами свяжется менеджер"');
+if ((total || ordered) === null)
+{
+    if (total < ordered) {
+    console.log('"На складе недостаточно товаров!"');
+    } else {
+    console.log('"Заказ оформлен, с вами свяжется менеджер"');
+    }
 }
